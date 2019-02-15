b6e4acbf8ef5ec45ba87719f467eda21ef1b0c8b
Quentin SAVEAN <quentin.savean@gmail.com>
2019-02-15 14:26:02 +0100

    Delete rules file

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bot = new Discord.Client()
 const YoutubeStream = require('youtube-audio-stream')
 const Play = require('./commands/play')
 const Leave = require('./commands/leave')
-const Rules = require('./commands/rules')
 
 bot.on('ready', function() {
     bot.user.setActivity('Fortnite').catch(console.error)
@@ -102,6 +101,44 @@ bot.on('message', function (message) {
   });
   }
  
+})
+
+bot.on('message', function (message) {
+    if(message.content === '!commandes') {
+         message.channel.send({embed: {
+        color: 3447003,
+        title: "Voici les differentes commandes du bot",
+        description: "D\'autres commandes et améliorations sont prévues ultérieurement",
+        fields: [
+            {
+                name: "!Regles",
+                value: "Énonce les règles du mini-jeu"
+            },
+            {
+            name: "!Start",
+            value: "Lance la partie"
+          },
+          {
+            name: "!Time [min:sec]",
+            value: "Modifie le laps de temps en minutes"
+          },
+          {
+            name: "!Avatar [Url]",
+            value: "Permet de modifier le logo du bot"
+          },
+          {
+            name: "!NameBot [nom]",
+            value: "Change le nom du bot"
+          }
+        ],
+        timestamp: new Date(),
+        footer: {
+          text: "© Quentin"
+        }
+      }
+    });
+    }
+   
 })
 
 bot.login(process.env.BOT_TOKEN);
