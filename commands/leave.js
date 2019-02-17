4592eafbd6eba76af374126fb7a702fccd8a85a9
Quentin SAVEAN <quentin.savean@gmail.com>
2019-02-17 17:17:17 +0100

    Update

--- commands/leave.js
+++ commands/leave.js
@@ -3,6 +3,7 @@ const Command = require('./command')
 module.exports = class Leave extends Command {
 
     static match (message) {
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n\'avez pas les permissions d\'utiliser cette commande !");
         return message.content.startsWith('!stop')
     }
 
