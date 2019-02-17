156fce86aa7d5f2e087e3459277a9ce6fbbab503
quenquen147 <quentin.savean@gmail.com>
2019-02-17 17:59:53 +0100

    Update and rename play.js to start.js

---
+++ commands/start.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'\avez pas les permissions pour utiliser cette commande !");
+  let voiceChannel =  message.guild.channels
+  .filter(function (channel) { return channel.id === '345949387769249794' })
+  .first()
+  voiceChannel
+  .join()
+  
+  .then(function (connection) {
+       connection.playFile('./mp3/SonsZone.mp3');
+     }) 
+   //  console.log(message.guild.channels.array());
+}
+
+module.exports.help = {
+  name:"start"
+}
