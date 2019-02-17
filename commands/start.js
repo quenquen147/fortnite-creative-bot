5f4d6f89252ec39102a49504e63ca94b1dc7baa0
quenquen147 <quentin.savean@gmail.com>
2019-02-17 18:02:36 +0100

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Vous n'\avez pas les permissions pour utiliser cette commande !");
   let voiceChannel =  message.guild.channels
-  .filter(function (channel) { return channel.id === '345949387769249794' })
+  .filter(function (channel) { return channel.id === '540093524570406912' })
   .first()
   voiceChannel
   .join()
