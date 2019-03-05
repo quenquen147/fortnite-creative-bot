260528d27afee4491ee2d648e3366fdfcb622e0a
Quentin <quentin.savean@gmail.com>
2019-03-05 20:52:17 +0100

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -7,6 +7,7 @@ module.exports.run = async (client, message, args) => {
   .first()
   voiceChannel
   .join()
+  .speakable(false)
   
   .then(function (connection) {
        connection.playFile('./mp3/SonsZone.mp3');
