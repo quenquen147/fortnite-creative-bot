022c0a3b2557a61f7f8070efed1f38df3d74f40b
Quentin <quentin.savean@gmail.com>
2019-03-08 20:47:02 +0100

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, message, args) => {
   .filter(function (channel) { return channel.id === '540093524570406912' })
   .first()
   voiceChannel
-  .join().members.setMute(true)
+  .join()
   
   .then(function (connection) {
        connection.playFile('./mp3/SonsZone.mp3');
