8004a9f03df4474d170a9cdd5148010e6d25366f
Quentin <quentin.savean@gmail.com>
2019-03-09 20:23:57 +0100

    Update start.js

--- commands/start.js
+++ commands/start.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
   .join()
   .then(function (connection) {
       connection.playFile('./mp3/SonsZone.mp3');
-      connection.members.setMute(true);
+      connection.voiceChannel.members.setMute(true);
      })
    //  console.log(message.guild.channels.array());
 }
